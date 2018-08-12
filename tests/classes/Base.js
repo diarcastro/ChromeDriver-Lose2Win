99d97a5413f948c4fc4d232519e1ee7c77c2cfad
Diego Castro <ing.diegocastro@gmail.com>
2018-08-12 17:01:09 -0500

    Update base.js

--- tests/classes/Base.js
+++ tests/classes/Base.js
@@ -2,7 +2,7 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 
 class Base {
     constructor () {
-        this.driver = new Builder()
+        this.driver = await new Builder()
             .forBrowser('chrome')
             .build();
     }
